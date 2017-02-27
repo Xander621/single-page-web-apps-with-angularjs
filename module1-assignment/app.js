1d3b8a7a46477e69e2ffe30485c60d2b83a4ffda
Alex Lance <alexander.lance@gmail.com>
2017-02-26 18:32:51 -0700

    Cleaned up some lazy logic

--- module1-assignment/app.js
+++ module1-assignment/app.js
@@ -8,32 +8,26 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController ($scope) {
   var emptyFieldFound = false;
-  var removedEmpty = "Remove invalid empty entries.";
   $scope.buttonClick = function () {
-    // Grab the text string and split on the comma
+    var removedEmpty = " Removed invalid empty entries.";
+    emptyFieldFound = false;
     // Check to see if there is anything to evaluate
     if($scope.lunchDishes === undefined) {
       $scope.lunchMessage = "There is nothing to evaluate!";
     } else {
-      // var count = splitString($scope.lunchDishes);
+      var msg = "";
       var count = splitString($scope.lunchDishes);
-      // $scope.lunchMessage = "count is " + count;
-      if(count === 0) {
-        $scope.lunchMessage = "Please enter valid entries."
+      if(count == 0) {
+        msg = "Please enter data first."
       } else if(count <= 3) {
-        if(emptyFieldFound) {
-          $scope.lunchMessage = "Enjoy!  Removed empty entries.";
-
-        } else {
-          $scope.lunchMessage = "Enjoy!";
-        }
+        msg = "Enjoy!";
       } else {
-        if(emptyFieldFound) {
-          $scope.lunchMessage = "Too much!  Removed empty entries.";
-
-        } else {
-          $scope.lunchMessage = "Too much!";        }
+        msg = "Too much!";
       }
+      if(emptyFieldFound) {
+        msg += removedEmpty;
+      }
+      $scope.lunchMessage = msg;
     }
   };
 
